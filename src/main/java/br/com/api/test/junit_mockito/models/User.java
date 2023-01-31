5e70edb5db6c822a714d4ee4bdb3ecf416e3e683
TaylanTorres09 <taylantorres@gmail.com>
2023-01-31 14:05:36 -0300

    Create Model User

---
+++ src/main/java/br/com/api/test/junit_mockito/models/User.java
@@ -0,0 +1,29 @@
+package br.com.api.test.junit_mockito.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+    
+}
