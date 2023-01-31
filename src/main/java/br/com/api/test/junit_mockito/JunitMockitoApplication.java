99c5bf4e435fbd3f94b966ea6e606bd379b01942
TaylanTorres09 <taylantorres@gmail.com>
2023-01-31 13:30:40 -0300

    Create project to train test with JUnit5 and Mockito

---
+++ src/main/java/br/com/api/test/junit_mockito/JunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.test.junit_mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/test/junit_mockito/JunitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.test.junit_mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
