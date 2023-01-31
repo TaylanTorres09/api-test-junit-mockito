367b501577ba2544cc4f349ce3651dd94a719030
TaylanTorres09 <taylantorres@gmail.com>
2023-01-31 14:14:54 -0300

    Create Controller

---
+++ src/main/java/br/com/api/test/junit_mockito/controllers/UserController.java
@@ -0,0 +1,27 @@
+package br.com.api.test.junit_mockito.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.test.junit_mockito.models.User;
+import br.com.api.test.junit_mockito.services.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+    
+}
