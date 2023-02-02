2906c2f227414010b1ed8e99db0a7f8ad6aa564f
TaylanTorres09 <taylantorres@gmail.com>
2023-02-02 16:23:29 -0300

    Instatiate Mapper when initialize project

---
+++ src/main/java/br/com/api/test/junit_mockito/config/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package br.com.api.test.junit_mockito.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+
+}
