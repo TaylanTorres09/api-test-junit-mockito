6bdfe0f08b8f41c6224d8b6ba15134e5aa7f9394
TaylanTorres09 <taylantorres@gmail.com>
2023-02-02 17:03:48 -0300

    Methods to create User

--- src/main/java/br/com/api/test/junit_mockito/controllers/UserController.java
+++ src/main/java/br/com/api/test/junit_mockito/controllers/UserController.java
@@ -1,5 +1,6 @@
 package br.com.api.test.junit_mockito.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
@@ -8,8 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.api.test.junit_mockito.dtos.UserDTO;
 import br.com.api.test.junit_mockito.models.User;
@@ -36,6 +40,15 @@ public class UserController {
         List<User> users = this.userService.findAll();
         List<UserDTO> userDTOs = users.stream().map(user -> mapper.map(user, UserDTO.class)).toList();
         return new ResponseEntity<List<UserDTO>>(userDTOs, HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody UserDTO userDTO) {
+        User user = this.userService.create(userDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/user/{id}").buildAndExpand(user.getId()).toUri();
+        
+        //return uri in headers
+        return ResponseEntity.created(uri).build();
     }
     
 }
--- src/main/java/br/com/api/test/junit_mockito/services/UserService.java
+++ src/main/java/br/com/api/test/junit_mockito/services/UserService.java
@@ -2,9 +2,11 @@ package br.com.api.test.junit_mockito.services;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.api.test.junit_mockito.dtos.UserDTO;
 import br.com.api.test.junit_mockito.models.User;
 import br.com.api.test.junit_mockito.repositories.UserRepository;
 import br.com.api.test.junit_mockito.services.exceptions.ObjectNotFoundException;
@@ -15,6 +17,9 @@ public class UserService {
     @Autowired 
     private UserRepository userRepository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     public User findById(Long id) {
         return this.userRepository.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("User id: " + id + ", not Found."));
@@ -22,6 +27,10 @@ public class UserService {
 
     public List<User> findAll() {
         return this.userRepository.findAll();
+    }
+
+    public User create(UserDTO userDTO) {
+        return this.userRepository.save(mapper.map(userDTO, User.class));
     }
 
 }
