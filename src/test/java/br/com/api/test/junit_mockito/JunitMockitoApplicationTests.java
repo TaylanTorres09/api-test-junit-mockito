d3150d824023cf44e87429355f4e75934e826a22
TaylanTorres09 <taylantorres@gmail.com>
2023-02-03 14:00:32 -0300

    test for principal class

--- src/test/java/br/com/api/test/junit_mockito/JunitMockitoApplicationTests.java
+++ src/test/java/br/com/api/test/junit_mockito/JunitMockitoApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class JunitMockitoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		JunitMockitoApplication.main(new String[] {});
 	}
 
 }
