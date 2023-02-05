3641e035fc5838c173a6cd41b612add94712967a
TaylanTorres09 <taylantorres@gmail.com>
2023-02-05 07:19:17 -0300

    Create file test to UserController

---
+++ src/test/java/br/com/api/test/junit_mockito/controllers/UserControllerTest.java
@@ -0,0 +1,30 @@
+package br.com.api.test.junit_mockito.controllers;
+
+import org.junit.jupiter.api.Test;
+
+public class UserControllerTest {
+    @Test
+    void testCreate() {
+
+    }
+
+    @Test
+    void testDelete() {
+
+    }
+
+    @Test
+    void testFindAll() {
+
+    }
+
+    @Test
+    void testFindById() {
+
+    }
+
+    @Test
+    void testUpdate() {
+
+    }
+}
