88645b6acabf48bb4b5b2ab05a149bf92b4085b1
TaylanTorres09 <taylantorres@gmail.com>
2023-02-05 07:26:14 -0300

    add setUp and instances

--- src/test/java/br/com/api/test/junit_mockito/controllers/UserControllerTest.java
+++ src/test/java/br/com/api/test/junit_mockito/controllers/UserControllerTest.java
@@ -1,8 +1,33 @@
 package br.com.api.test.junit_mockito.controllers;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.modelmapper.ModelMapper;
+
+import br.com.api.test.junit_mockito.dtos.UserDTO;
+import br.com.api.test.junit_mockito.models.User;
+import br.com.api.test.junit_mockito.services.UserService;
 
 public class UserControllerTest {
+
+    @InjectMocks
+    private UserController userController;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private ModelMapper mapper;
+
+    private User user;
+    private UserDTO userDTO;
+
+    @BeforeEach
+    void setUp() {
+    }
+
     @Test
     void testCreate() {
 
